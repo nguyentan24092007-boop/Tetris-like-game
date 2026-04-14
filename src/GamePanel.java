9ae067c8362d3532829c7a618c17431f9e1c307f
Nguyễn Tuấn Anh <ITITIU25002@student.hcmiu.edu.vn>
2026-04-14 11:46:29 +0700

    Add files via upload

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,47 @@
-public class GamePanel {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    public static final int WIDTH=1020;
+    public static final int HEIGHT=720;
+    final int FPS = 60;
+    Thread gameThread;
+
+    public GamePanel(){
+        this.setBackground(Color.BLACK);
+        this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        this.setLayout(null);
+    }
+    public void launchGame(){
+         gameThread = new Thread(this);
+         gameThread.start();
+
+    }
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta=0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime-lastTime)/drawInterval;
+            lastTime=currentTime;
+
+            if (delta >=1 ){
+                update();
+                repaint();
+                delta--;
+            }
+        }
+
+    }
+    public void update(){
+
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
+import javax.swing.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Simple Tetris");
+
+        //Add GamePanel to Window
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
 }

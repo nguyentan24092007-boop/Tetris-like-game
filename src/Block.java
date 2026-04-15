271d0a0c4360a80658582812c16cb6e2b161fb90
TanN <nguyentan24092007@gmail.com>
2026-04-16 01:00:58 +0700

    Add Block class to track if a square is empty and what color it is

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,24 @@
+import java.awt.Color;
+
 public class Block {
-    
+    private Color color;
+    private boolean haveSquare;
+
+    public Block() {
+        this.color = Color.BLACK;
+        this.haveSquare = false;
+    }
+    public boolean hasPiece() {
+        return this.haveSquare;
+    }
+    public Color getColor() { return this.color; }
+
+    public void have(Color newColor) {
+        this.haveSquare = true;
+        this.color = newColor;
+    }
+    public void dontHave() {
+        this.haveSquare = false;
+        this.color = Color.BLACK;
+    }
 }

170f1e6273e10f58cdc61a721646584e04120047
TanN <nguyentan24092007@gmail.com>
2026-04-24 22:54:34 +0700

    create a tetromino test

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,14 +7,26 @@ public class GamePanel extends JPanel implements Runnable {
     final int FPS = 60;
     Thread gameThread;
 
+
+    private Board board;
+    public static int blockSize = 30;
+    private Tetromino tetromino;
+
+
     public GamePanel(){
         this.setBackground(Color.BLACK);
         this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
         this.setLayout(null);
+
     }
     public void launchGame(){
-         gameThread = new Thread(this);
-         gameThread.start();
+        gameThread = new Thread(this);
+        gameThread.start();
+
+        //cai nay de thu, se xoa sau
+        this.board = new Board();
+        int blueSquare[][] = { {1,1},{1,1}} ;
+        this.tetromino = new Tetromino(blueSquare, Color.BLUE);
 
     }
     @Override
@@ -38,10 +50,35 @@ public class GamePanel extends JPanel implements Runnable {
 
     }
     public void update(){
-
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
+        if(board != null) {
+            Block[][] current = board.getGrid();
+            
+            for(int i = 0; i < Board.column; i++) {
+                for(int j = 0; j< Board.row; j++) {
+                    if(current[i][j].hasPiece()) {
+                        g.setColor(current[i][j].getColor());
+                        g.fillRect(i*blockSize, j*blockSize, blockSize, blockSize);
+                    }
+                }
+            }
+        }
+        if(tetromino != null) {
+            int[][] shape = tetromino.getShape();
+            g.setColor(tetromino.getColor());
+
+            for(int i = 0; i < shape.length; i++) {
+                for(int j = 0; j < shape[0].length; j++) {
+                    if(shape[i][j] == 1) {
+                        int x = (tetromino.getX() + j)*blockSize;
+                        int y = (tetromino.getY() + i)*blockSize;
+                        g.fillRect (x, y, blockSize, blockSize);
+                    }
+                }
+            }
+        }
     }
 }

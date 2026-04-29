689eda8d5bf3984add6d58d30452c589253830b5
TanN <nguyentan24092007@gmail.com>
2026-04-30 03:08:18 +0700

    Implement Factory Pattern for tetromino

---
+++ src/RandomTetromino.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.util.*;
+
+public class RandomTetromino {
+    public static Tetromino randomShape() {
+        int[][][] allShape = {
+            {{1,1},{1,1}},
+            {{1,1,1,1}},
+            {{0,1,0},{1,1,1}},
+            {{1,1,0},{0,1,1}},
+            {{0,1,1},{1,1,0}},
+            {{1,0,0},{1,1,1}},
+            {{0,0,1},{1,1,1}}
+        };
+        Color[] allColor = {
+            Color.CYAN,
+            Color.RED,
+            Color.ORANGE,
+            Color.YELLOW,
+            Color.BLUE,
+            Color.PINK,
+            Color.GREEN
+        };
+        Random random = new Random();
+        int roller = random.nextInt(7);
+        return new Tetromino(allShape[roller], allColor[roller]);
+    }
+}

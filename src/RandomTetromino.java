b855ae97e90b3a4a0f3d05c96c57b4804d2a2eff
Nguyễn Tuấn Anh <ITITIU25002@student.hcmiu.edu.vn>
2026-05-06 23:48:12 +0700

    Change the color of tetrominos

--- src/RandomTetromino.java
+++ src/RandomTetromino.java
@@ -4,22 +4,22 @@ import java.util.*;
 public class RandomTetromino {
     public static Tetromino randomShape() {
         int[][][] allShape = {
-            {{1,1},{1,1}}, //O
-            {{0,0,0,0},{1,1,1,1},{0,0,0,0},{0,0,0,0}}, //I
-            {{0,1,0},{1,1,1},{0,0,0}}, //T
-            {{1,1,0},{0,1,1},{0,0,0}}, //Z
-            {{0,1,1},{1,1,0},{0,0,0}}, //S
-            {{1,0,0},{1,1,1},{0,0,0}}, //J
-            {{0,0,1},{1,1,1},{0,0,0}}  //L
+                {{1,1},{1,1}}, //O
+                {{0,0,0,0},{1,1,1,1},{0,0,0,0},{0,0,0,0}}, //I
+                {{0,1,0},{1,1,1},{0,0,0}}, //T
+                {{1,1,0},{0,1,1},{0,0,0}}, //Z
+                {{0,1,1},{1,1,0},{0,0,0}}, //S
+                {{1,0,0},{1,1,1},{0,0,0}}, //J
+                {{0,0,1},{1,1,1},{0,0,0}}
         };
         Color[] allColor = {
-            Color.CYAN,
-            Color.RED,
-            Color.ORANGE,
-            Color.YELLOW,
-            Color.BLUE,
-            Color.PINK,
-            Color.GREEN
+                new Color(255, 230, 0),
+                new Color(0, 255, 255),
+                new Color(191, 0, 255),
+                new Color(255, 20, 147),
+                new Color(50, 255, 18),
+                new Color(0, 102, 255),
+                new Color(255, 140, 0)
         };
         Random random = new Random();
         int roller = random.nextInt(7);

2a3264dfbcbd6a3db85b35dfac488fa66ba8b163
HuyZu207 <huyvu0372007@gmail.com>
2026-05-06 20:46:38 +0700

    Add sfx code

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -24,9 +24,11 @@ public class KeyInput extends KeyAdapter {
     //game over
     private void gameOverInput(int input) {
         if( input == KeyEvent.VK_ENTER) {
+            SFX.playSound("sfx/menu_SFX.wav");
             gamePanel.resetGame();
         }
         else if( input == KeyEvent.VK_ESCAPE) {
+            SFX.playSound("sfx/menu_SFX.wav");
             gamePanel.setCurrentState(GamePanel.GameState.MENU);
             gamePanel.setGameOver(false);
         }
@@ -36,22 +38,25 @@ public class KeyInput extends KeyAdapter {
     private void menuInput(int key) {
         int menuOption = gamePanel.getMenuOption();
         if (key == KeyEvent.VK_UP) {
+            SFX.playSound("sfx/menu_SFX.wav");
             menuOption--;
             if (menuOption < 0) menuOption = 3;
             gamePanel.setMenuOption(menuOption);
         }
         else if (key == KeyEvent.VK_DOWN) {
+            SFX.playSound("sfx/menu_SFX.wav");
             menuOption++;
             if (menuOption > 3) menuOption = 0;
             gamePanel.setMenuOption(menuOption);
-        } 
+        }
         else if (key == KeyEvent.VK_ENTER) {
+            SFX.playSound("sfx/menu_SFX.wav");
             if (menuOption == 0) gamePanel.resetGame();
             else if (menuOption == 1) {
                 if (gamePanel.getFallSpeed() == 30) { gamePanel.setFallSpeed(15); gamePanel.setDifficulty("HARD"); }
                 else if (gamePanel.getFallSpeed() == 15) { gamePanel.setFallSpeed(45); gamePanel.setDifficulty("EASY"); }
                 else { gamePanel.setFallSpeed(30); gamePanel.setDifficulty("NORMAL"); }
-            } 
+            }
             else if (menuOption == 2) { //pick mode
                 if (gamePanel.getGameModeNumber() == 0) {
                     gamePanel.setGameModeNumber(1);
@@ -83,30 +88,35 @@ public class KeyInput extends KeyAdapter {
                 if(gamePanel.getBoard().valid(gamePanel.getTetromino(), x-1, y)) {
                     gamePanel.getTetromino().move(-1, 0);
                 }
+                SFX.playSound("sfx/rotate_block.wav"); // rotating block sfx
                 break;
             case KeyEvent.VK_RIGHT:
                 if(gamePanel.getBoard().valid(gamePanel.getTetromino(), x+1, y)) {
                     gamePanel.getTetromino().move(1, 0);
                 }
+                SFX.playSound("sfx/rotate_block.wav"); // rotating block sfx
                 break;
             case KeyEvent.VK_DOWN:
                 if(gamePanel.getBoard().valid(gamePanel.getTetromino(), x, y+1)) {
                     gamePanel.getTetromino().move(0, 1);
                 }
+                SFX.playSound("sfx/rotate_block.wav"); // rotating block sfx
                 break;
             case KeyEvent.VK_UP:
                 if (gamePanel.getGameModeNumber() == 2) {
                     gamePanel.randomShape();
-                } 
+                }
                 else {
                     gamePanel.rotateLogic();
                 }
+                SFX.playSound("sfx/rotate_block.wav"); // rotating block sfx
                 break;
             case KeyEvent.VK_SPACE:  //instant drop
                 while(gamePanel.getBoard().valid(gamePanel.getTetromino(), gamePanel.getTetromino().getX(), gamePanel.getTetromino().getY() + 1)) {
                     gamePanel.getTetromino().move(0, 1);
                 }
-                gamePanel.setFallCount(30); 
+                gamePanel.setFallCount(30);
+
                 break;
         }
     }

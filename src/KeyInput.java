1a56a156c7b6bef6d44a5ae643f8ce3134bc7ca6
Nguyễn Tuấn Anh <ITITIU25002@student.hcmiu.edu.vn>
2026-05-07 00:15:49 +0700

    Add key input for pause and return to menu

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -3,6 +3,7 @@ import java.awt.event.KeyEvent;
 
 public class KeyInput extends KeyAdapter {
     private GamePanel gamePanel;
+
     public KeyInput(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
     }
@@ -17,16 +18,17 @@ public class KeyInput extends KeyAdapter {
             menuInput(input);
         } else if (gamePanel.getCurrentState() == GamePanel.GameState.PLAYING) {
             gamePlayInput(input);
+        } else if (gamePanel.getCurrentState() == GamePanel.GameState.PAUSED) {
+            pausedInput(input);
         }
         gamePanel.repaint();
     }
 
     //game over
     private void gameOverInput(int input) {
-        if( input == KeyEvent.VK_ENTER) {
+        if (input == KeyEvent.VK_ENTER) {
             gamePanel.resetGame();
-        }
-        else if( input == KeyEvent.VK_ESCAPE) {
+        } else if (input == KeyEvent.VK_ESCAPE) {
             gamePanel.setCurrentState(GamePanel.GameState.MENU);
             gamePanel.setGameOver(false);
         }
@@ -39,40 +41,41 @@ public class KeyInput extends KeyAdapter {
             menuOption--;
             if (menuOption < 0) menuOption = 3;
             gamePanel.setMenuOption(menuOption);
-        }
-        else if (key == KeyEvent.VK_DOWN) {
+        } else if (key == KeyEvent.VK_DOWN) {
             menuOption++;
             if (menuOption > 3) menuOption = 0;
             gamePanel.setMenuOption(menuOption);
-        } 
-        else if (key == KeyEvent.VK_ENTER) {
+        } else if (key == KeyEvent.VK_ENTER) {
             if (menuOption == 0) gamePanel.resetGame();
             else if (menuOption == 1) {
-                if (gamePanel.getFallSpeed() == 30) { gamePanel.setFallSpeed(15); gamePanel.setDifficulty("HARD"); }
-                else if (gamePanel.getFallSpeed() == 15) { gamePanel.setFallSpeed(45); gamePanel.setDifficulty("EASY"); }
-                else { gamePanel.setFallSpeed(30); gamePanel.setDifficulty("NORMAL"); }
-            } 
-            else if (menuOption == 2) { //pick mode
+                if (gamePanel.getFallSpeed() == 30) {
+                    gamePanel.setFallSpeed(15);
+                    gamePanel.setDifficulty("HARD");
+                } else if (gamePanel.getFallSpeed() == 15) {
+                    gamePanel.setFallSpeed(45);
+                    gamePanel.setDifficulty("EASY");
+                } else {
+                    gamePanel.setFallSpeed(30);
+                    gamePanel.setDifficulty("NORMAL");
+                }
+            } else if (menuOption == 2) { //pick mode
                 if (gamePanel.getGameModeNumber() == 0) {
                     gamePanel.setGameModeNumber(1);
                     gamePanel.setModeName("POTHOLE");
-                }
-                else if(gamePanel.getGameModeNumber() == 1) {
+                } else if (gamePanel.getGameModeNumber() == 1) {
                     gamePanel.setGameModeNumber(2);
                     gamePanel.setModeName("RANDOM SHAPE");
-                }
-                else {
+                } else {
                     gamePanel.setGameModeNumber(0);
                     gamePanel.setModeName("CLASSIC");
                 }
-            }
-            else if (menuOption == 3) System.exit(0);
+            } else if (menuOption == 3) System.exit(0);
         }
     }
 
     //gameplay
     private void gamePlayInput(int input) {
-        if(gamePanel.getTetromino() == null || gamePanel.getBoard() == null) {
+        if (gamePanel.getTetromino() == null || gamePanel.getBoard() == null) {
             return;
         }
         int x = gamePanel.getTetromino().getX();
@@ -80,34 +83,44 @@ public class KeyInput extends KeyAdapter {
 
         switch (input) {
             case KeyEvent.VK_LEFT:
-                if(gamePanel.getBoard().valid(gamePanel.getTetromino(), x-1, y)) {
+                if (gamePanel.getBoard().valid(gamePanel.getTetromino(), x - 1, y)) {
                     gamePanel.getTetromino().move(-1, 0);
                 }
                 break;
             case KeyEvent.VK_RIGHT:
-                if(gamePanel.getBoard().valid(gamePanel.getTetromino(), x+1, y)) {
+                if (gamePanel.getBoard().valid(gamePanel.getTetromino(), x + 1, y)) {
                     gamePanel.getTetromino().move(1, 0);
                 }
                 break;
             case KeyEvent.VK_DOWN:
-                if(gamePanel.getBoard().valid(gamePanel.getTetromino(), x, y+1)) {
+                if (gamePanel.getBoard().valid(gamePanel.getTetromino(), x, y + 1)) {
                     gamePanel.getTetromino().move(0, 1);
                 }
                 break;
             case KeyEvent.VK_UP:
                 if (gamePanel.getGameModeNumber() == 2) {
                     gamePanel.randomShape();
-                } 
-                else {
+                } else {
                     gamePanel.rotateLogic();
                 }
                 break;
             case KeyEvent.VK_SPACE:  //instant drop
-                while(gamePanel.getBoard().valid(gamePanel.getTetromino(), gamePanel.getTetromino().getX(), gamePanel.getTetromino().getY() + 1)) {
+                while (gamePanel.getBoard().valid(gamePanel.getTetromino(), gamePanel.getTetromino().getX(), gamePanel.getTetromino().getY() + 1)) {
                     gamePanel.getTetromino().move(0, 1);
                 }
-                gamePanel.setFallCount(30); 
+                gamePanel.setFallCount(30);
                 break;
+        }
+        if (input == KeyEvent.VK_ESCAPE) {
+            gamePanel.setCurrentState(GamePanel.GameState.PAUSED);
+        }
+    }
+
+    private void pausedInput(int input) {
+        if (input == KeyEvent.VK_ESCAPE || input == KeyEvent.VK_ENTER) {
+            gamePanel.setCurrentState(GamePanel.GameState.PLAYING);
+        } else if (input == KeyEvent.VK_BACK_SPACE) {
+            gamePanel.setCurrentState(GamePanel.GameState.MENU);
         }
     }
 }

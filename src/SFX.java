49ec71c05df721f90f5e2dfaae60602f57ecffad
TanN <nguyentan24092007@gmail.com>
2026-05-09 04:12:46 +0700

    Fix sound path

--- src/SFX.java
+++ src/SFX.java
@@ -2,13 +2,13 @@ import javax.sound.sampled.*;
 import java.io.File;
 
 public class SFX {
-    public static String THEME_BGM = "sfx/theme.wav"; 
-    public static String GAMEOVER_BGM = "sfx/game_over.wav";
-    public static String MOVE_SFX = "sfx/move.wav"; // when move block and change option in menu
-    public static String ROTATE_SFX = "sfx/rotate.wav"; // press up
-    public static String CLEAR_SFX = "sfx/clear.wav"; // line clear
-    public static String DROP_SFX = "sfx/drop.wav"; //for instance drop (press space)
-    public static String SELECT_SFX = "sfx/select.wav"; // press enter, esc, backspace
+    public static String THEME_BGM = "tetris-like-game/sfx/theme.wav"; 
+    public static String GAMEOVER_BGM = "tetris-like-game/sfx/game_over.wav";
+    public static String MOVE_SFX = "tetris-like-game/sfx/move.wav"; // when move block and change option in menu
+    public static String ROTATE_SFX = "tetris-like-game/sfx/rotate.wav"; // press up
+    public static String CLEAR_SFX = "tetris-like-game/sfx/clear.wav"; // line clear
+    public static String DROP_SFX = "tetris-like-game/sfx/drop.wav"; //for instance drop (press space)
+    public static String SELECT_SFX = "tetris-like-game/sfx/select.wav"; // press enter, esc, backspace
 
     public static void playSound(String filePath) {
         try {
